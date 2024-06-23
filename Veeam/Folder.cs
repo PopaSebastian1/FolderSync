544f278335e00cd5f2b59196e51d2138d1be7237
PopaSebastian1 <sebastian.popa@student.unitbv.ro>
2024-06-23 20:28:38 +0300

    Add project files.

---
+++ Veeam/Folder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Veeam
+{
+    public class Folder
+    {
+        public string Path { get; set; }
+
+        public Folder(string path)
+        {
+            Path = path;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public List<FileInfo> GetFiles()
+        {
+            return new DirectoryInfo(Path).GetFiles("*.*", SearchOption.AllDirectories).ToList();
+        }
+
+        public List<FileInfo> GetModifiedFiles(DateTime date)
+        {
+            return new DirectoryInfo(Path).GetFiles("*.*", SearchOption.AllDirectories).Where(x => x.LastWriteTime > date).ToList();
+        }
+
+    }
+}
